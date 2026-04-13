3fad827847938711a8efe1fb164ee8155f7ce627
Zabedfolio <zabedfolio@gmail.com>
2026-04-14 05:45:51 +0600

    added navbar and footer

---
+++ src/app/friends/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetails = () => {
+    return (
+        <div>
+            This is friends detail page
+        </div>
+    );
+};
+
+export default FriendDetails;
---
+++ src/app/friends/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            this is friends
+        </div>
+    );
+};
+
+export default Friends;
